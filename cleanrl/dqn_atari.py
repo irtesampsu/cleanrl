6fe8eea40dc55bf7ec53b4a768e51a909da72d55
irtesampsu <ixk5174@psu.edu>
2025-09-26 18:48:24 -0400

    added gaussian perturbation

--- cleanrl/dqn_atari.py
+++ cleanrl/dqn_atari.py
@@ -105,24 +105,24 @@ def make_env(env_id, seed, idx, capture_video, run_name):
 
 
 # ALGO LOGIC: initialize agent here:
-class QNetwork(nn.Module):
-    def __init__(self, env):
-        super().__init__()
-        self.network = nn.Sequential(
-            nn.Conv2d(4, 32, 8, stride=4),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, 4, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, 3, stride=1),
-            nn.ReLU(),
-            nn.Flatten(),
-            nn.Linear(3136, 512),
-            nn.ReLU(),
-            nn.Linear(512, env.single_action_space.n),
+class QNetwork(nn.Module): # Neural network for learning Q value as a  parameterized function
+    def __init__(self, env): # initialization
+        super().__init__() # initialization of the super class
+        self.network = nn.Sequential( # Sequential module is used to create a pipeline, an atari 'rgb' obs has 
+            nn.Conv2d(4, 32, 8, stride=4), # 4 input channels, 32 output channels, kernel size of 8 and stride 4
+            nn.ReLU(), # relu activation 
+            nn.Conv2d(32, 64, 4, stride=2), # 32 input channels, 64 output channels, kernel size of 4 and stride 2
+            nn.ReLU(), # relu activation 
+            nn.Conv2d(64, 64, 3, stride=1), #64 input channels, 64 output channels, kernel size of 3 and stride 1
+            nn.ReLU(), # relu activation
+            nn.Flatten(), # flattened all dimensions to 1d
+            nn.Linear(3136, 512), # fully connected layer with 3136 input channels and 512 output channels
+            nn.ReLU(), # relu activation
+            nn.Linear(512, env.single_action_space.n), # fully connected layer with 512 inputs and number of action outputs
         )
 
     def forward(self, x):
-        return self.network(x / 255.0)
+        return self.network(x / 255.0) # clipped by 255 to keep all values between 0 and 1
 
 
 def linear_schedule(start_e: float, end_e: float, duration: int, t: int):
@@ -166,37 +166,48 @@ if __name__ == "__main__":
     )
     assert isinstance(envs.single_action_space, gym.spaces.Discrete), "only discrete action space is supported"
 
-    q_network = QNetwork(envs).to(device)
-    optimizer = optim.Adam(q_network.parameters(), lr=args.learning_rate)
-    target_network = QNetwork(envs).to(device)
-    target_network.load_state_dict(q_network.state_dict())
+    q_network = QNetwork(envs).to(device) # defined a Q network
+    optimizer = optim.Adam(q_network.parameters(), lr=args.learning_rate) # defined adam optimizer
+    target_network = QNetwork(envs).to(device) # defined the target network
+    target_network.load_state_dict(q_network.state_dict()) # defined target network to be equal to the q_network
 
-    rb = ReplayBuffer(
+    rb = ReplayBuffer( 
         args.buffer_size,
         envs.single_observation_space,
         envs.single_action_space,
         device,
         optimize_memory_usage=True,
         handle_timeout_termination=False,
-    )
-    start_time = time.time()
+    ) # replay buffer for keeping track of recent transitions
+    start_time = time.time() 
 
     # TRY NOT TO MODIFY: start the game
-    obs, _ = envs.reset(seed=args.seed)
-    for global_step in range(args.total_timesteps):
+    obs, _ = envs.reset(seed=args.seed) # env initialized
+    for global_step in range(args.total_timesteps): # loop over a predefined number of steps
         # ALGO LOGIC: put action logic here
-        epsilon = linear_schedule(args.start_e, args.end_e, args.exploration_fraction * args.total_timesteps, global_step)
-        if random.random() < epsilon:
-            actions = np.array([envs.single_action_space.sample() for _ in range(envs.num_envs)])
-        else:
-            q_values = q_network(torch.Tensor(obs).to(device))
-            actions = torch.argmax(q_values, dim=1).cpu().numpy()
+        epsilon = linear_schedule(args.start_e, args.end_e, args.exploration_fraction * args.total_timesteps, global_step) # a linear schedule for epsilon
+        # based on the slope it can either increase or decrease with timesteps
+        # if random.random() < epsilon:  # eps-greedy strategy for exploration
+        #     actions = np.array([envs.single_action_space.sample() for _ in range(envs.num_envs)]) # choose a random action
+        # else:
+        #     q_values = q_network(torch.Tensor(obs).to(device)) # get q values
+        #     actions = torch.argmax(q_values, dim=1).cpu().numpy() # choose the most probable action based on q-values
+        
+        # implement genetic algorithm on probability distribution
+        q_values = q_network(torch.Tensor(obs).to(device))
+        prob = F.softmax(q_values, dim=0)
+        mean = 0.0
+        stddev = 0.2
+        noise = torch.rand(prob.shape)*mean + stddev
+        actions = torch.argmax(prob+noise, dim=1).cpu().numpy()
+
+
 
         # TRY NOT TO MODIFY: execute the game and log data.
-        next_obs, rewards, terminations, truncations, infos = envs.step(actions)
+        next_obs, rewards, terminations, truncations, infos = envs.step(actions) # take the action and observe
 
         # TRY NOT TO MODIFY: record rewards for plotting purposes
-        if "final_info" in infos:
+        if "final_info" in infos: 
             for info in infos["final_info"]:
                 if info and "episode" in info:
                     print(f"global_step={global_step}, episodic_return={info['episode']['r']}")
@@ -204,42 +215,42 @@ if __name__ == "__main__":
                     writer.add_scalar("charts/episodic_length", info["episode"]["l"], global_step)
 
         # TRY NOT TO MODIFY: save data to reply buffer; handle `final_observation`
-        real_next_obs = next_obs.copy()
+        real_next_obs = next_obs.copy() 
         for idx, trunc in enumerate(truncations):
             if trunc:
                 real_next_obs[idx] = infos["final_observation"][idx]
-        rb.add(obs, real_next_obs, actions, rewards, terminations, infos)
+        rb.add(obs, real_next_obs, actions, rewards, terminations, infos) # add the transition to replay buffer
 
         # TRY NOT TO MODIFY: CRUCIAL step easy to overlook
-        obs = next_obs
+        obs = next_obs # take a step; change the current observation to the new one
 
         # ALGO LOGIC: training.
-        if global_step > args.learning_starts:
-            if global_step % args.train_frequency == 0:
-                data = rb.sample(args.batch_size)
-                with torch.no_grad():
-                    target_max, _ = target_network(data.next_observations).max(dim=1)
-                    td_target = data.rewards.flatten() + args.gamma * target_max * (1 - data.dones.flatten())
-                old_val = q_network(data.observations).gather(1, data.actions).squeeze()
-                loss = F.mse_loss(td_target, old_val)
+        if global_step > args.learning_starts: # start learning after a certain period
+            if global_step % args.train_frequency == 0: # train only after few time steps
+                data = rb.sample(args.batch_size) # take a minibatch sample from replay buffer
+                with torch.no_grad(): # do not calculate gradient for target network yet
+                    target_max, _ = target_network(data.next_observations).max(dim=1) # max q-value from target network
+                    td_target = data.rewards.flatten() + args.gamma * target_max * (1 - data.dones.flatten()) # calculate target value
+                old_val = q_network(data.observations).gather(1, data.actions).squeeze() # calculate value from the current q network
+                loss = F.mse_loss(td_target, old_val) # calculate mse loss between the two values
 
-                if global_step % 100 == 0:
+                if global_step % 100 == 0: 
                     writer.add_scalar("losses/td_loss", loss, global_step)
                     writer.add_scalar("losses/q_values", old_val.mean().item(), global_step)
                     print("SPS:", int(global_step / (time.time() - start_time)))
                     writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
 
                 # optimize the model
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
+                optimizer.zero_grad() # reset optimizer after each step in training
+                loss.backward() # back propagation
+                optimizer.step() # update the model based on loss
 
             # update target network
-            if global_step % args.target_network_frequency == 0:
-                for target_network_param, q_network_param in zip(target_network.parameters(), q_network.parameters()):
+            if global_step % args.target_network_frequency == 0: # only update target network after large number of steps
+                for target_network_param, q_network_param in zip(target_network.parameters(), q_network.parameters()): # iterate over all parameters of the model
                     target_network_param.data.copy_(
                         args.tau * q_network_param.data + (1.0 - args.tau) * target_network_param.data
-                    )
+                    ) # copy the q_network to the target network
 
     if args.save_model:
         model_path = f"runs/{run_name}/{args.exp_name}.cleanrl_model"
